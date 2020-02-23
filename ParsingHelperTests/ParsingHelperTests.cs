23345950698e6b13bfeca93091027119ba78840b
Jonathan Wood <jwood@softcircuits.com>
2020-02-23 11:38:24 -0700

    1.0.6; Added ParseTo() methods; Reworked and expanded unit tests; Optimizations and fixes.

--- ParsingHelperTests/ParsingHelperTests.cs
+++ ParsingHelperTests/ParsingHelperTests.cs
@@ -12,16 +12,34 @@ namespace ParsingHelperTests
     [TestClass]
     public class ParsingHelperTests
     {
+        private const string Alphabet = "Abcdefghijklmnopqrstuvwxyz";
+        private const string Numbers = "1234567890";
+        private const string TestString = @"Four score and seven years ago our fathers brought forth on this continent,
+a new nation, conceived in Liberty, and dedicated to the proposition that all men are created equal.
+
+Now we are engaged in a great civil war, testing whether that nation, or any nation so conceived and so
+dedicated, can long endure. We are met on a great battle-field of that war. We have come to dedicate a
+portion of that field, as a final resting place for those who here gave their lives that that nation might
+live. It is altogether fitting and proper that we should do this.
+
+But, in a larger sense, we can not dedicate -- we can not consecrate -- we can not hallow -- this ground.
+The brave men, living and dead, who struggled here, have consecrated it, far above our poor power to add or
+detract. The world will little note, nor long remember what we say here, but it can never forget what they
+did here. It is for us the living, rather, to be dedicated here to the unfinished work which they who fought
+here have thus far so nobly advanced. It is rather for us to be here dedicated to the great task remaining
+before us -- that from these honored dead we take increased devotion to that cause for which they gave the
+last full measure of devotion -- that we here highly resolve that these dead shall not have died in vain --
+that this nation, under God, shall have a new birth of freedom -- and that government of the people, by the
+people, for the people, shall not perish from the earth.";
+
         [TestMethod]
         public void BasicTests()
         {
-            string testString = "Abcdefghijklmnopqrstuvwxyz";
-            ParsingHelper helper = new ParsingHelper(testString);
+            ParsingHelper helper = new ParsingHelper(Alphabet);
 
             Assert.AreEqual('\0', ParsingHelper.NullChar);
-
+            Assert.AreEqual(Alphabet, helper.Text);
             Assert.AreEqual(0, helper.Index);
-            Assert.AreEqual(testString, helper.Text);
 
             Assert.AreEqual('A', helper.Peek());
             Assert.AreEqual('b', helper.Peek(1));
@@ -29,141 +47,181 @@ namespace ParsingHelperTests
             Assert.AreEqual(0, helper.Index);
 
             helper.Next();
+            Assert.AreEqual(1, helper.Index);
             Assert.AreEqual('b', helper.Peek());
             helper.Next(2);
+            Assert.AreEqual(3, helper.Index);
             Assert.AreEqual('d', helper.Peek());
-            helper.Skip('f', 'e', 'd');
-            Assert.AreEqual('g', helper.Peek());
+            helper.Next(-2);
+            Assert.AreEqual(1, helper.Index);
+            Assert.AreEqual('b', helper.Peek());
 
-            helper.SkipTo("mno");
-            Assert.AreEqual('m', helper.Peek());
-            Assert.AreEqual(12, helper.Index);
-            helper.SkipTo('u', 't', 'v');
-            Assert.AreEqual('t', helper.Peek());
-            Assert.AreEqual(19, helper.Index);
+            Assert.AreEqual(false, helper.EndOfText);
+            Assert.AreEqual(helper.Text.Length - helper.Index, helper.Remaining);
 
-            helper.SkipTo('X');
-            Assert.AreEqual(26, helper.Index);
+            helper.Next(-10000);
+            Assert.AreEqual(0, helper.Index);
+            Assert.AreEqual(false, helper.EndOfText);
+            Assert.AreEqual(helper.Text.Length - helper.Index, helper.Remaining);
+
+            helper.Next(10000);
+            Assert.AreEqual(helper.Text.Length, helper.Index);
+            Assert.AreEqual(true, helper.EndOfText);
+            Assert.AreEqual(0, helper.Remaining);
+
+            helper.Reset();
+            Assert.AreEqual(0, helper.Index);
+            Assert.AreEqual(Alphabet, helper.Text);
+
+            helper.Reset(Numbers);
+            Assert.AreEqual(Numbers, helper.Text);
+            Assert.AreEqual('1', helper.Peek());
+            Assert.AreEqual('2', helper.Peek(1));
+            Assert.AreEqual(ParsingHelper.NullChar, helper.Peek(1000));
+            Assert.AreEqual(0, helper.Index);
+
+            helper.Reset(null);
+            Assert.AreEqual(0, helper.Index);
+            Assert.AreEqual(string.Empty, helper.Text);
+        }
+
+        [TestMethod]
+        public void SkipTests()
+        {
+            ParsingHelper helper = new ParsingHelper(TestString);
+
+            helper.SkipTo("score");
+            Assert.AreEqual('s', helper.Peek());
+
+            helper.Reset();
+            helper.SkipTo("SCORE", StringComparison.OrdinalIgnoreCase);
+            Assert.AreEqual('s', helper.Peek());
+
+            helper.Reset();
+            helper.SkipTo('v');
+            Assert.AreEqual('v', helper.Peek());
+            Assert.AreEqual('e', helper.Peek(1));
+            Assert.AreEqual('n', helper.Peek(2));
+
+            helper.SkipToNextLine();
+            Assert.AreEqual('a', helper.Peek());
+            Assert.AreEqual(' ', helper.Peek(1));
+
+            helper.SkipToEndOfLine();
+            Assert.AreEqual('\r', helper.Peek());
+            Assert.AreEqual('\n', helper.Peek(1));
+
+            helper.SkipTo("XxXxXxX");
+            Assert.AreEqual(helper.Text.Length, helper.Index);
+            Assert.AreEqual(true, helper.EndOfText);
+
+            helper.Reset();
+            helper.SkipTo(' ');
+            helper.SkipWhiteSpace();
+            Assert.AreEqual('s', helper.Peek());
+
+            helper.SkipWhile(c => "score".Contains(c));
+            Assert.AreEqual(' ', helper.Peek());
+            Assert.AreEqual('a', helper.Peek(1));
+        }
+
+        [TestMethod]
+        public void ParseTests()
+        {
+            ParsingHelper helper = new ParsingHelper(TestString);
+
+            helper.SkipTo("score");
+            string s = helper.ParseTo("fathers");
+            Assert.AreEqual('f', helper.Peek());
+            Assert.AreEqual("score and seven years ago our ", s);
+
+            helper.Reset();
+            helper.SkipTo("score");
+            s = helper.ParseTo("FATHERS", StringComparison.OrdinalIgnoreCase);
+            Assert.AreEqual('f', helper.Peek());
+            Assert.AreEqual("score and seven years ago our ", s);
+
+            helper.Reset();
+            helper.SkipTo("score");
+            s = helper.ParseTo('v', 'X', 'Y', 'Z');
+            Assert.AreEqual('v', helper.Peek());
+            Assert.AreEqual("score and se", s);
+
+            helper.Reset();
+            helper.SkipTo("score");
+            s = helper.ParseWhile(c => c != ',');
+            Assert.AreEqual(',', helper.Peek());
+            Assert.AreEqual("score and seven years ago our fathers brought forth on this continent", s);
+
+            helper.Next();  // Skip comma
+            s = helper.ParseToken(char.IsWhiteSpace);
+            Assert.AreEqual(' ', helper.Peek());
+            Assert.AreEqual('n', helper.Peek(1));
+            Assert.AreEqual("a", s);
+
+            helper.Reset();
+            s = helper.ParseToken(' ', '\r', '\n');
+            Assert.AreEqual(' ', helper.Peek());
+            Assert.AreEqual("Four", s);
+        }
+
+        [TestMethod]
+        public void QuotedTextTests()
+        {
+            ParsingHelper helper = new ParsingHelper("He turned and said, \"Yes.\"");
+            helper.SkipTo('"');
+            string s = helper.ParseQuotedText();
+            Assert.AreEqual("Yes.", s);
+            Assert.AreEqual(helper.Text.Length, helper.Index);
             Assert.AreEqual(ParsingHelper.NullChar, helper.Peek());
 
-            helper.Next(-1000);
-            Assert.AreEqual(0, helper.Index);
-
-            helper.Reset();
-            Assert.AreEqual(0, helper.Index);
-            Assert.AreEqual(testString, helper.Text);
-        }
-
-        [TestMethod]
-        public void AdvancedTests()
-        {
-            string testString = "Once upon a time, in a \r\n" +
-                "land far, far away, there was small boy named \"Henry\".";
-            ParsingHelper helper = new ParsingHelper(testString);
-
-            helper.SkipTo("time");
-            Assert.AreEqual('t', helper.Peek());
-            Assert.IsTrue(helper.MatchesCurrentPosition("time"));
-
-            // New line methods
-            helper.SkipToEndOfLine();
-            Assert.AreEqual('\r', helper.Peek());
-
-            helper.SkipToNextLine();
-            Assert.AreEqual('l', helper.Peek());
-
-            helper.Reset();
-            helper.SkipToEndOfLine();
-            Assert.AreEqual('\r', helper.Peek());
-            helper++;   // Within new line
-
-            helper.SkipToNextLine();
-            Assert.AreEqual('l', helper.Peek());
-
-            helper.Reset();
-            helper.SkipToNextLine();
-            Assert.AreEqual('l', helper.Peek());
-
-            helper.SkipTo("land");
-            Assert.IsTrue(helper.MatchesCurrentPosition("land"));
-            helper += "land".Length;
-            helper.SkipWhiteSpace();
-            Assert.AreEqual('f', helper.Peek());
-
-            string s = helper.ParseWhile(c => !char.IsWhiteSpace(c) && c != ',');
-            Assert.AreEqual("far", s);
-            Assert.AreEqual(',', helper.Peek());
-
-            helper.SkipTo("named");
-            helper.Next("named".Length);
-            helper.SkipWhiteSpace();
-            Assert.AreEqual('\"', helper.Peek());
+            helper = new ParsingHelper("He turned and said, \"I turned and said, \"\"Yes\"\".\"");
+            helper.SkipTo('"');
             s = helper.ParseQuotedText();
-            Assert.AreEqual("Henry", s);
-            Assert.AreEqual('.', helper.Peek());
+            Assert.AreEqual("I turned and said, \"Yes\".", s);
+            Assert.AreEqual(helper.Text.Length, helper.Index);
+            Assert.AreEqual(ParsingHelper.NullChar, helper.Peek());
 
-            helper.Reset();
-            helper.SkipTo("UPON", StringComparison.OrdinalIgnoreCase);
+            helper = new ParsingHelper("He turned and said, 'I turned and said, ''Yes''.'");
+            helper.SkipTo('\'');
+            s = helper.ParseQuotedText();
+            Assert.AreEqual("I turned and said, 'Yes'.", s);
+            Assert.AreEqual(helper.Text.Length, helper.Index);
+            Assert.AreEqual(ParsingHelper.NullChar, helper.Peek());
         }
 
         [TestMethod]
-        public void CountWordsTest()
+        public void MatchesCurrentPositionTests()
         {
-            string testString = "Once upon a time, in a \r\n" +
-                "land far, far away, there was small boy named \"Henry\".";
-            int words = 0;
-            char[] wordChars = "abcdefghijklmnopqrstuvwxyz'".ToCharArray();
-            char[] delimiters = { ' ', '\r', '\n', '.', ',', '"' };
+            ParsingHelper helper = new ParsingHelper(TestString);
+            helper.SkipTo("consecrated it");
+            Assert.AreEqual(true, helper.MatchesCurrentPosition("consecrated it"));
+            Assert.AreEqual(true, helper.MatchesCurrentPosition("CONSECRATED IT", StringComparison.OrdinalIgnoreCase));
+        }
 
-            ParsingHelper helper = new ParsingHelper(testString);
-
-            while (true)
-            {
-                helper.SkipWhile(c => !wordChars.Contains(c));
-                if (helper.EndOfText)
-                    break;
-                helper.SkipWhile(c => wordChars.Contains(c));
-                words++;
-            }
-            Assert.AreEqual(16, words);
-
-            words = 0;
-            helper.Reset();
-            while (!helper.EndOfText)
-            {
-                string token = helper.ParseToken(delimiters);
-                if (token.Length > 0)
-                    words++;
-            }
-            Assert.AreEqual(16, words);
-
-            words = 0;
-            helper.Reset();
-            while (!helper.EndOfText)
-            {
-                string token = helper.ParseToken(c => delimiters.Contains(c));
-                if (token.Length > 0)
-                    words++;
-            }
-            Assert.AreEqual(16, words);
-
-            helper.Reset();
-            Assert.AreEqual("Once", helper.ParseToken(delimiters));
-            Assert.AreEqual("upon", helper.ParseToken(delimiters));
-            Assert.AreEqual("a", helper.ParseToken(delimiters));
-            Assert.AreEqual("time", helper.ParseToken(delimiters));
-            Assert.AreEqual("in", helper.ParseToken(delimiters));
-            Assert.AreEqual("a", helper.ParseToken(delimiters));
-            Assert.AreEqual("land", helper.ParseToken(delimiters));
+        [TestMethod]
+        public void ExtractTests()
+        {
+            ParsingHelper helper = new ParsingHelper(TestString);
+            string s = "consecrated it";
+            helper.SkipTo(s);
+            int start = helper.Index;
+            helper.Next(s.Length);
+            Assert.AreEqual(s, helper.Extract(start, helper.Index));
+            Assert.AreEqual(@"consecrated it, far above our poor power to add or
+detract. The world will little note, nor long remember what we say here, but it can never forget what they
+did here. It is for us the living, rather, to be dedicated here to the unfinished work which they who fought
+here have thus far so nobly advanced. It is rather for us to be here dedicated to the great task remaining
+before us -- that from these honored dead we take increased devotion to that cause for which they gave the
+last full measure of devotion -- that we here highly resolve that these dead shall not have died in vain --
+that this nation, under God, shall have a new birth of freedom -- and that government of the people, by the
+people, for the people, shall not perish from the earth.", helper.Extract(start));
         }
 
         [TestMethod]
         public void OperatorOverloadTest()
         {
-            string testString = "Once upon a time, in a \r\n" +
-                "land far, far away, there was small boy named \"Henry\".";
-
-            ParsingHelper helper = new ParsingHelper(testString);
+            ParsingHelper helper = new ParsingHelper(TestString);
 
             int i = 0;
             while (!helper.EndOfText)
--- SoftCircuits.Parsing.Helper/ParsingHelper.cs
+++ SoftCircuits.Parsing.Helper/ParsingHelper.cs
@@ -61,20 +61,23 @@ namespace SoftCircuits.Parsing.Helper
         /// <summary>
         /// Indicates if the current position is at the end of the text being parsed.
         /// </summary>
-        public bool EndOfText => (Index >= Text.Length);
+        public bool EndOfText => Index >= Text.Length;
 
         /// <summary>
         /// Returns the number of characters not yet parsed. This is equal to the length of the
         /// text being parsed minus the current position within that text.
         /// </summary>
-        public int Remaining => (Text.Length - Index);
+        public int Remaining => Text.Length - Index;
 
         /// <summary>
         /// Returns the character at the current position, or <see cref="NullChar"></see> if
         /// we're at the end of the text being parsed.
         /// </summary>
         /// <returns>The character at the current position.</returns>
-        public char Peek() => Peek(0);
+        public char Peek()
+        {
+            return (Index >= 0 && Index < Text.Length) ? Text[Index] : NullChar;
+        }
 
         /// <summary>
         /// Returns the character at the specified number of characters beyond the current
@@ -105,13 +108,11 @@ namespace SoftCircuits.Parsing.Helper
         /// to move back.</param>
         public void Next(int count)
         {
-            int index = Index + count;
-            if (index < 0)
+            Index += count;
+            if (Index < 0)
                 Index = 0;
-            else if (index > Text.Length)
+            else if (Index > Text.Length)
                 Index = Text.Length;
-            else
-                Index = index;
         }
 
         /// <summary>
@@ -120,18 +121,19 @@ namespace SoftCircuits.Parsing.Helper
         /// current position to the end of the input text and returns <c>false</c>.
         /// </summary>
         /// <param name="s">String to find.</param>
+        /// <param name="comparison">One of the enumeration values that specifies the rules for
+        /// search.</param>
         /// <returns>Returns a Boolean value that indicates if any of the specified characters
         /// were found.</returns>
         public bool SkipTo(string s, StringComparison comparison = StringComparison.Ordinal)
         {
-            int index = Text.IndexOf(s, Index, comparison);
-            if (index >= 0)
+            Index = Text.IndexOf(s, Index, comparison);
+            if (Index == -1)
             {
-                Index = index;
-                return true;
+                Index = Text.Length;
+                return false;
             }
-            Index = Text.Length;
-            return false;
+            return true;
         }
 
         /// <summary>
@@ -144,15 +146,16 @@ namespace SoftCircuits.Parsing.Helper
         /// were found.</returns>
         public bool SkipTo(params char[] chars)
         {
-            int index = Text.IndexOfAny(chars, Index);
-            if (index >= 0)
+            Index = Text.IndexOfAny(chars, Index);
+            if (Index == -1)
             {
-                Index = index;
-                return true;
+                Index = Text.Length;
+                return false;
             }
-            Index = Text.Length;
-            return false;
+            return true;
         }
+
+        private static readonly char[] NewLineChars = { '\r', '\n' };
 
         /// <summary>
         /// Moves the current position forward to the start of the next line.
@@ -161,12 +164,12 @@ namespace SoftCircuits.Parsing.Helper
         /// found.</returns>
         public bool SkipToNextLine()
         {
-            // Move to start of next line (if not already at a new line)
+            // Move to start of next new line (if not already at a new line)
             char c = Peek();
-            if (c != '\r' && c != '\n')
+            if (c != NewLineChars[0] && c != NewLineChars[1])
                 SkipToEndOfLine();
             // Move to end of new line
-            if (Peek() == '\r' && Peek(1) == '\n')
+            if (Peek() == NewLineChars[0] && Peek(1) == NewLineChars[1])
                 Next();
             Next();
             return !EndOfText;
@@ -177,7 +180,7 @@ namespace SoftCircuits.Parsing.Helper
         /// </summary>
         /// <returns>Returns a Boolean value that indicates if any newline characters
         /// were found.</returns>
-        public bool SkipToEndOfLine() => SkipTo('\r', '\n');
+        public bool SkipToEndOfLine() => SkipTo(NewLineChars);
 
         /// <summary>
         /// Moves the current position to the next character that is not whitespace.
@@ -209,42 +212,93 @@ namespace SoftCircuits.Parsing.Helper
         }
 
         /// <summary>
+        /// Parses characters until the next occurrence of the specified string and returns a
+        /// string with the parsed characters. If the specified string is not found, this method parses
+        /// all character to the end of the input text.
+        /// </summary>
+        /// <param name="s">String to parse until.</param>
+        /// <param name="comparison">One of the enumeration values that specifies the rules for
+        /// search.</param>
+        /// <returns>A string with the characters parsed.</returns>
+        public string ParseTo(string s, StringComparison comparison = StringComparison.Ordinal)
+        {
+            int start = Index;
+            Index = Text.IndexOf(s, Index, comparison);
+            if (Index == -1)
+                Index = Text.Length;
+            return Text.Substring(start, Index - start);
+        }
+
+        /// <summary>
+        /// Parses characters until the next occurrence of any one of the specified characters and
+        /// returns a string with the parsed characters. If none of the specified characters are found,
+        /// this method parses all character to the end of the input text.
+        /// </summary>
+        /// <param name="chars">Characters to parse until.</param>
+        /// <returns>A string with the characters parsed.</returns>
+        public string ParseTo(params char[] chars)
+        {
+            int start = Index;
+            Index = Text.IndexOfAny(chars, Index);
+            if (Index == -1)
+                Index = Text.Length;
+            return Text.Substring(start, Index - start);
+        }
+
+        /// <summary>
+        /// Parses characters until the next character for which <paramref name="predicate"/> returns
+        /// true. If no such characters are found, this method parses all character to the end of the
+        /// input text.
+        /// </summary>
+        /// <param name="predicate">Function that returns true when parsing should stop.</param>
+        /// <returns>A string with the characters parsed.</returns>
+        public string ParseTo(Func<char, bool> predicate)
+        {
+            int start = Index;
+            while (!EndOfText && !predicate(Peek()))
+                Next();
+            return Text.Substring(start, Index - start);
+        }
+
+        /// <summary>
         /// Parses characters until the next character that causes <paramref name="predicate"/> to
         /// return false, and then returns the characters spanned. Can return an empty string.
         /// </summary>
         /// <param name="predicate">Function to test each character.</param>
+        /// <returns>A string with the characters parsed.</returns>
         public string ParseWhile(Func<char, bool> predicate)
         {
             int start = Index;
             while (!EndOfText && predicate(Peek()))
                 Next();
-            return Extract(start, Index);
+            return Text.Substring(start, Index - start);
         }
 
         /// <summary>
-        /// Skips any characters that are in the list of delimiters, and then parses any
-        /// characters that are not in the list of delimiters. Returns the parsed
-        /// characters.
+        /// Parses text using the specified delimiter characters. Skips any characters that are in the
+        /// list of delimiters, and then parses any characters that are not in the list of delimiters.
+        /// Returns the parsed characters.
         /// </summary>
-        /// <param name="delimiters">Delimiter characters.</param>
+        /// <param name="delimiters">Token delimiter characters.</param>
         /// <returns>Returns the parsed token.</returns>
         public string ParseToken(params char[] delimiters)
         {
-            SkipWhile(c => delimiters.Contains(c));
-            return ParseWhile(c => !delimiters.Contains(c));
+            Skip(delimiters);
+            return ParseTo(delimiters);
         }
 
         /// <summary>
-        /// Skips any characters for which <paramref name="predicate"/> returns true, and
-        /// then parses any characters for which <paramref name="predicate"/> returns false.
-        /// Returns the parsed characters.
+        /// Parses text using the specified predicate to indicate delimiter characters. Skips any
+        /// characters for which <paramref name="predicate"/> returns true, and then parses any
+        /// characters for which <paramref name="predicate"/> returns false. Returns the parsed
+        /// characters.
         /// </summary>
-        /// <param name="predicate">Function that returns true for delimiter characters.</param>
+        /// <param name="predicate">Function that returns true for token delimiter characters.</param>
         /// <returns>Returns the parsed token.</returns>
         public string ParseToken(Func<char, bool> predicate)
         {
-            SkipWhile(c => predicate(c));
-            return ParseWhile(c => !predicate(c));
+            SkipWhile(predicate);
+            return ParseTo(predicate);
         }
 
         /// <summary>
@@ -264,11 +318,9 @@ namespace SoftCircuits.Parsing.Helper
             while (!EndOfText)
             {
                 int start = Index;
-                // Move to next quote
-                SkipTo(quote);
-                // Capture quoted text
-                builder.Append(Extract(start, Index));
-                // Skip over quote
+                // Parse to next quote
+                builder.Append(ParseWhile(c => c != quote));
+                // Skip quote
                 Next();
                 // Two consecutive quotes treated as quote literal
                 if (Peek() == quote)
@@ -290,7 +342,7 @@ namespace SoftCircuits.Parsing.Helper
         /// Returns false otherwise.</returns>
         public bool MatchesCurrentPosition(string s, StringComparison comparison = StringComparison.Ordinal)
         {
-            // TODO: Rewrite to use ReadOnlySpan<char> when available
+            // TODO: Can we rewrite to use ReadOnlySpan<char> when available?
             if (s == null || s.Length == 0 || Remaining < s.Length)
                 return false;
             return s.Equals(Text.Substring(Index, s.Length), comparison);
@@ -302,7 +354,7 @@ namespace SoftCircuits.Parsing.Helper
         /// </summary>
         /// <param name="start">0-based position of first character to be extracted.</param>
         /// <returns>Returns the extracted string.</returns>
-        public string Extract(int start) => Extract(start, Text.Length);
+        public string Extract(int start) => Text.Substring(start, Text.Length - start);
 
         /// <summary>
         /// Extracts a substring from the specified range of the text being parsed.
